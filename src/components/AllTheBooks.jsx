ed5cb7be9c736a1906c1f94aed1c67b97fc724bf
Avenez <145536778+Avenez@users.noreply.github.com>
2023-12-19 16:34:46 +0100

    books

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,20 +1,32 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+import Container from 'react-bootstrap/esm/Container';
+import fantasyBooks from '../data/books/fantasy.json';
+import Row from 'react-bootstrap/esm/Row';
 
 class AllTheBooks extends React.Component {
     render(){
   return (
-    <Card>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <Container>
+        <Row>
+            {fantasyBooks.map((book) =>(
+                    <Card id={book.id}>
+                    <Card.Img variant="top" src={book.img} />
+                    <Card.Body>
+                      <Card.Title>{book.title}</Card.Title>
+                      <Card.Text>
+                        <Badge bg="warning"> {book.price}€</Badge>
+                        <Badge bg="info">{book.category}</Badge>
+                      </Card.Text>
+                      {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                  </Card>
+            ))}
+
+    </Row>
+    </Container>
   );
 }
 }
